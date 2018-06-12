a97f89acb803b085cb680c195f35b1fadbf78bd2
curquiza <clementine.urquizar@gmail.com>
2018-06-12 11:16:58 +0200

    add first architecture

---
+++ includes/alloc_dym.h
@@ -0,0 +1,8 @@
+#ifndef ALLOC_DYM_H
+# define ALLOC_DYM_H
+
+# include <unistd.h>
+// # include <stdlib.h>
+// # include <stdio.h>
+
+#endif
---
+++ srcs/ft_malloc.c
@@ -0,0 +1,7 @@
+#include "alloc_dym.h"
+
+void	*ft_malloc(size_t size)
+{
+	(void)size;
+	return ((void *)"coco");
+}
