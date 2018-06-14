b930f2b8ea9df999dd6c62ee1e512650c966b977
curquiza <clementine.urquizar@gmail.com>
2018-06-14 14:28:07 +0200

    fix sizes in extend function

--- srcs/malloc.c
+++ srcs/malloc.c
@@ -5,9 +5,18 @@ t_base	g_bases = { NULL, NULL, NULL };
 t_block	*extend_heap(size_t size, t_block *previous)
 {
 	t_block	*b;
+	size_t	size_to_map;
+	size_t	mmap_size;
 
-	b = mmap(0, size, PROT_READ | PROT_WRITE, MAP_ANON | MAP_PRIVATE, -1, 0);
-	b->size = (size / page_size()) * page_size() + page_size();
+	size_to_map = sizeof_header() + size;
+	mmap_size = ((size_to_map  / page_size()) + 1) * page_size();
+	ft_putendl("EXTEND HEAP");
+	ft_putnbr2("Size needed = size + sizeof_header = ", size_to_map);
+	ft_putnbr2("mmap_size = ", mmap_size);
+	b = mmap(0, size_to_map, PROT_READ | PROT_WRITE, MAP_ANON | MAP_PRIVATE, -1, 0);
+	b->size = mmap_size - sizeof_header();
+	if (size > b->size)
+		ft_putendl("Extend size error !");
 	b->status = FREE;
 	b->next = NULL;
 	if (previous)
@@ -60,6 +69,14 @@ t_block	*find_or_extend(t_block **blocks, size_t size)
 		return (extend_heap(size, place));
 }
 
+// t_block	*split_block(t_block *block, size_t size)
+// {
+// 	t_block		*new_block;
+
+// 	new_block = (unsigned char *)
+// 	block->size = size;
+// }
+
 void	display_all_blocks(t_block *blocks)
 {
 	int		i;
@@ -72,7 +89,7 @@ void	display_all_blocks(t_block *blocks)
 		ft_putstr("address = ");
 		ft_display_addr((unsigned long long)blocks);
 		ft_putnbr2("size of header = ", sizeof_header());
-		ft_putnbr2("size of block = ", blocks->size);
+		ft_putnbr2("size of block (without header) = ", blocks->size);
 		ft_putendl2("status = ", blocks->status == FREE ? "FREE" : "ALLOC");
 		ft_putendl("---------");
 		blocks = blocks->next;
@@ -90,6 +107,7 @@ void	*ft_malloc(size_t size)
 	else
 		ft_putendl("Next init");
 	alloc_b = find_or_extend(&g_bases.tiny, size);
+	ft_putendl("");
 	display_all_blocks(g_bases.tiny);
 	ft_putendl("--- END MALLOC ------------------\n");
 
