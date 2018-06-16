f188fa1e55445c8c60f8aca3304fa1a0493c568b
Clurquiz <clementine.urquizar@gmail.com>
2018-06-16 19:46:09 +0200

    upd main with test0 and test1

--- main.c
+++ main.c
@@ -1,9 +1,36 @@
 #include "dyn_alloc.h"
 
+void	test0(void)
+{
+	int i;
+	char *addr;
+
+	(void)addr;
+	i = 0;
+	while (i < 1024)
+	{
+		i++;
+	}
+}
+
+void	test1(void)
+{
+	int i;
+	char *addr;
+
+	i = 0;
+	while (i < 1024)
+	{
+		addr = (char*)malloc(1024);
+		addr[0] = 42;
+		i++;
+	}
+}
+
 int		main(void)
 {
-	char * a = malloc(-1);(void)a;
-	char * b = malloc(0);(void)b;
+	// char * a = malloc(-1);(void)a;
+	// char * b = malloc(0);(void)b;
 
 	// char * a = malloc(255);(void)a;
 	// char * b = malloc(4100); (void)b;
@@ -11,6 +38,9 @@ int		main(void)
 	// char * d = malloc(0); (void)d;
 	// char * e = malloc(0); (void)e;
 	// char * f = malloc(20); (void)f;
+
+	// test0();
+	test1();
 
 	// while(1);
 	return (0);
