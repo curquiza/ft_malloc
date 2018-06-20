d8d210fbd37fcf7851ceaeaa2cd33f5e10d0db4f
curquiza <clementine.urquizar@gmail.com>
2018-06-20 16:30:32 +0200

    free in progress;

--- srcs/free.c
+++ srcs/free.c
@@ -5,17 +5,21 @@ static t_block	*look_for_addr_on(t_block *blocks, void *ptr)
 	while (blocks)
 	{
 		if ((unsigned char *)blocks + sizeof_header() == (unsigned char *)ptr)
-			return (true);
+			return (blocks);
 		blocks = blocks->next;
 	}
-	return (false);
+	return (NULL);
 }
 
-static	free_on()
+static void	free_on(t_block *block)
+{
+	(void)block;
+}
 
 void	free(void *ptr)
 {
 	t_block		*b_to_free;
+
 	ft_putstr(B_GREEN"FREE"DEF" - addr : ");
 	ft_display_addr((unsigned long long)ptr);
 	ft_putstr("\n");
@@ -31,7 +35,7 @@ void	free(void *ptr)
 			b_to_free = look_for_addr_on(g_zone.tiny, ptr);
 			if (b_to_free == NULL)
 			{
-				ft_putendl("FREE ABORT") // abort()
+				ft_putendl("FREE ABORT"); // abort()
 				return ;
 			}
 		}
