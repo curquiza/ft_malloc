2b2f8ac15ace42af6ae896b31a62ac446323e464
curquiza <clementine.urquizar@gmail.com>
2018-06-22 15:58:58 +0200

    upd main test with realloc

--- tests/main.c
+++ tests/main.c
@@ -1,9 +1,27 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+// int		main(void)
+// {
+// 	char	*a;
+// 	int		i;
+
+// 	a = (char *)malloc(10);
+// 	i = 0;
+// 	while (i < 10)
+// 	{
+// 		a[i] = 'c';
+// 		i++;
+// 	}
+// 	//free(a + 1); // breakpoint malloc
+// 	//free(a - 1); // breakpoint malloc
+// 	free(a); //ok
+// }
+
 int		main(void)
 {
 	char	*a;
+	char	*b;
 	int		i;
 
 	a = (char *)malloc(10);
@@ -13,7 +31,11 @@ int		main(void)
 		a[i] = 'c';
 		i++;
 	}
-	//free(a + 1); // breakpoint malloc
-	//free(a - 1); // breakpoint malloc
-	free(a); //ok
+	printf("a avant realloc = %p\n", a);	
+	// b = realloc(a, 16);
+	b = realloc(a, 20);
+	printf("a apres realloc = %p\n", a);
+	printf("afficher a = %s\n", a);
+	printf("b = %p\n", b);
+	printf("afficher b = %s\n", b);
 }
