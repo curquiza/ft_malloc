a73db2865c1e55a9c57c5e419f502ab64d8db130
curquiza <clementine.urquizar@gmail.com>
2018-06-22 18:39:39 +0200

    upd test main

--- tests/main.c
+++ tests/main.c
@@ -22,9 +22,10 @@ int		main(void)
 {
 	char	*a;
 	char	*b;
+	// char	*c;
 	int		i;
 
-	a = (char *)malloc(10);
+	a = (char *)malloc(8000);
 	i = 0;
 	while (i < 10)
 	{
@@ -33,9 +34,13 @@ int		main(void)
 	}
 	printf("a avant realloc = %p\n", a);	
 	// b = realloc(a, 16);
-	b = realloc(a, 20);
+	b = realloc(a, 200);
 	printf("a apres realloc = %p\n", a);
 	printf("afficher a = %s\n", a);
 	printf("b = %p\n", b);
 	printf("afficher b = %s\n", b);
+
+	// c = realloc(a, 20);
+	// printf("c = %p\n", c);
+	// printf("afficher a = %s\n", a); // des fois plante, des fois non
 }
