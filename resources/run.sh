b59d421972ebd6b87e108af20a9dd69ab4236df4
curquiza <clementine.urquizar@gmail.com>
2018-06-27 16:58:28 +0200

    upd remane

--- resources/run.sh
+++ resources/run.sh
@@ -2,7 +2,7 @@
 export DYLD_LIBRARY_PATH=.
 export DYLD_INSERT_LIBRARIES="libft_malloc.so"
 export DYLD_FORCE_FLAT_NAMESPACE=1
-export MALLOC42_DEBUG=1
-# export MALLOC42_NOCOLOR=1
+export ALLOC42_DEBUG=1
+# export ALLOC42_NOCOLOR=1
 # export SHOW_MEM_FREE=1
 $@
